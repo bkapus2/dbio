47ecae746eec277973657a46335358a40d8f5f17
bkapus2 <brian.kupi@gmail.com>
2017-11-05 20:22:46 -0600

    updated tests

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,150 +1,153 @@
 import dbio from '../lib/index.js';
 import { expect } from 'chai';
+import chai from 'chai';
+import sinon from 'sinon';
+import sinonChai from 'sinon-chai';
+chai.use(sinonChai);
 
 describe('dbio', function() {
 
+  const tableName = 'People';
+  const model = [
+    {
+      column: 'person_id',
+      key: 'personId',
+      type: 'INTEGER'
+    },
+    {
+      column: 'first_name',
+      key: 'firstName',
+      type: 'TEXT'
+    },
+    {
+      column: 'middle_name',
+      key: 'middleName',
+      type: 'TEXT'
+    },
+    {
+      column: 'last_name',
+      key: 'lastName',  
+      type: 'TEXT'
+    },
+    {
+      column: 'date_created',
+      key: 'dateCreated',  
+      type: 'TIMESTAMP'
+    },
+    // {
+    //   key: 'emails',
+    //   entity: 'email'
+    // },
+  ];
+
   it('should be an object', function() {
     expect(dbio).to.be.a('object');
-  })
+  });
 
   describe('#registerCollection', function() {
 
-    const tableName = 'People';
     const key = 'people';
-    const model = [
-      {
-        column: 'person_id',
-        key: 'personId',
-        type: 'INTEGER'
-      },
-      {
-        column: 'first_name',
-        key: 'firstName',
-        type: 'TEXT'
-      },
-      {
-        column: 'middle_name',
-        key: 'middleName',
-        type: 'TEXT'
-      },
-      {
-        column: 'last_name',
-        key: 'lastName',  
-        type: 'TEXT'
-      },
-      {
-        column: 'date_created',
-        key: 'dateCreated',  
-        type: 'TIMESTAMP'
-      },
-      // {
-      //   key: 'emails',
-      //   entity: 'email'
-      // },
-    ];
+
+    const registerCollection = function(...args) {
+      return function() {
+        return dbio.registerCollection(...args)
+      }
+    }
 
     afterEach(function(){
-      delete dbio.people
-    })
+      delete dbio[key]
+    });
 
     it('should be a function', function() {
       expect(dbio.registerCollection).to.be.a('function');
     });
 
-    it('should throw an error without a \'model\' argument', function() {
-      expect(()=>dbio.registerCollection({ key, tableName })).to.throw(Error, "model is not defined");
+    it('should throw an error when given incorrect arguments', function() {
+      expect(registerCollection({ key, tableName })).to.throw(Error, "model is not defined");
+      expect(registerCollection({ model: null, key, tableName })).to.throw(Error, "model is null");
+      expect(registerCollection({ model: 123, key, tableName })).to.throw(Error, "model is not of type array");
+      expect(registerCollection({ model: [null], key, tableName })).to.throw(Error, "prop is null");
+      expect(registerCollection({ model: [{ key: null, column: 'name', type: 'TEXT' }], key, tableName })).to.throw(Error, "key is not of type string");
+      expect(registerCollection({ model: [{ key: '', column: 'name', type: 'TEXT' }], key, tableName })).to.throw(Error, "key length is not atleast 1");
+      expect(registerCollection({ model: [{ key: 123, column: 'name', type: 'TEXT' }], key, tableName })).to.throw(Error, "key is not of type string");
+      expect(registerCollection({ model: [{ key: 'name', column: null, type: 'TEXT' }], key, tableName })).to.throw(Error, "column is not of type string");
+      expect(registerCollection({ model: [{ key: 'name', column: '', type: 'TEXT' }], key, tableName })).to.throw(Error, "column length is not atleast 1");
+      expect(registerCollection({ model: [{ key: 'name', column: 123, type: 'TEXT' }], key, tableName })).to.throw(Error, "column is not of type string");
+      expect(registerCollection({ model: [{ key: 'name', column: 'name', type: null }], key, tableName })).to.throw(Error, "type is not of type string");
+      expect(registerCollection({ model: [{ key: 'name', column: 'name', type: '' }], key, tableName })).to.throw(Error, "type is not an allowed value");
+      expect(registerCollection({ model: [{ key: 'name', column: 'name', type: 123 }], key, tableName })).to.throw(Error, "type is not of type string");
+      expect(registerCollection({ model: [{ key: 'name', column: 'name', type: 'asdf' }], key, tableName })).to.throw(Error, "type is not an allowed value");
+      expect(registerCollection({ model, tableName })).to.throw(Error, "key is not defined");
+      expect(registerCollection({ key: null, model, tableName })).to.throw(Error, "key is null");
+      expect(registerCollection({ key: 123, model, tableName })).to.throw(Error, "key is not of type string");
+      expect(registerCollection({ key: '', model, tableName })).to.throw(Error, "key length is not atleast 1");
+      expect(registerCollection({ key, model })).to.throw(Error, "tableName is not defined");
+      expect(registerCollection({ key, model, tableName: null })).to.throw(Error, "tableName is null");
+      expect(registerCollection({ key, model, tableName: 123 })).to.throw(Error, "tableName is not of type string");
+      expect(registerCollection({ key, model, tableName: '' })).to.throw(Error, "tableName length is not atleast 1");
     });
 
-    it('should throw an error with a bad \'model\' argument', function() {
-      expect(()=>dbio.registerCollection({ model: null, key, tableName })).to.throw(Error, "model is null");
+    it('should register valid arguments without error', function() {
+      const collection = dbio.registerCollection({ key, model, tableName });
+      expect(collection).to.be.an.instanceof(Object);
     });
 
-    it('should throw an error with a bad \'model\' argument', function() {
-      expect(()=>dbio.registerCollection({ model: 123, key, tableName })).to.throw(Error, "model is not of type array");
+    it('should correctly register all properties', function() {
+      const collection = dbio.registerCollection({ key, model, tableName });
+      expect(collection.constructor.name).to.equal(tableName);
+      expect(collection.model).to.equal(model);
+      expect(dbio[key]).to.equal(collection);
     });
+  });
 
-    it('should throw an error with a bad \'model\' argument', function() {
-      expect(()=>dbio.registerCollection({ model: [null], key, tableName })).to.throw(Error, "prop is null");
-    });
-
-    it('should throw an error with a bad \'model\' argument', function() {
-      expect(()=>dbio.registerCollection({ model: [{ key:null, column: 'name', type: 'TEXT' }], key, tableName })).to.throw(Error, "key is not of type string");
-    });
-
-    it('should throw an error with a bad \'model\' argument', function() {
-      expect(()=>dbio.registerCollection({ model: [{ key:'', column: 'name', type: 'TEXT' }], key, tableName })).to.throw(Error, "key length is not atleast 1");
-    });
-
-    it('should throw an error with a bad \'model\' argument', function() {
-      expect(()=>dbio.registerCollection({ model: [{ key:123, column: 'name', type: 'TEXT' }], key, tableName })).to.throw(Error, "key is not of type string");
-    });
-
-    it('should throw an error with a bad \'model\' argument', function() {
-      expect(()=>dbio.registerCollection({ model: [{ key:'name', column: null, type: 'TEXT' }], key, tableName })).to.throw(Error, "column is not of type string");
-    });
-
-    it('should throw an error with a bad \'model\' argument', function() {
-      expect(()=>dbio.registerCollection({ model: [{ key:'name', column: '', type: 'TEXT' }], key, tableName })).to.throw(Error, "column length is not atleast 1");
-    });
-
-    it('should throw an error with a bad \'model\' argument', function() {
-      expect(()=>dbio.registerCollection({ model: [{ key:'name', column: 123, type: 'TEXT' }], key, tableName })).to.throw(Error, "column is not of type string");
-    });
-
-    it('should throw an error with a bad \'model\' argument', function() {
-      expect(()=>dbio.registerCollection({ model: [{ key:'name', column: 'name', type: null }], key, tableName })).to.throw(Error, "type is not of type string");
-    });
-
-    it('should throw an error with a bad \'model\' argument', function() {
-      expect(()=>dbio.registerCollection({ model: [{ key:'name', column: 'name', type: '' }], key, tableName })).to.throw(Error, "type is not an allowed value");
-    });
-
-    it('should throw an error with a bad \'model\' argument', function() {
-      expect(()=>dbio.registerCollection({ model: [{ key:'name', column: 'name', type: 123 }], key, tableName })).to.throw(Error, "type is not of type string");
-    });
-
-    it('should throw an error with a bad \'model\' argument', function() {
-      expect(()=>dbio.registerCollection({ model: [{ key:'name', column: 'name', type: 'asdf' }], key, tableName })).to.throw(Error, "type is not an allowed value");
-    });
-
-    it('should throw an error without a \'key\' argument', function() {
-      expect(()=>dbio.registerCollection({ model, tableName })).to.throw(Error, "key is not defined");
-    });
-
-    it('should throw an error with an invalid \'key\' argument', function() {
-      expect(()=>dbio.registerCollection({ key: null, model, tableName })).to.throw(Error, "key is null");
-    });
-
-    it('should throw an error with an invalid \'key\' argument', function() {
-      expect(()=>dbio.registerCollection({ key: 123, model, tableName })).to.throw(Error, "key is not of type string");
-    });
-
-    it('should throw an error with an invalid \'key\' argument', function() {
-      expect(()=>dbio.registerCollection({ key: '', model, tableName })).to.throw(Error, "key length is not atleast 1");
-    });
-
-    it('should throw an error without a \'tableName\' argument', function() {
-      expect(()=>dbio.registerCollection({ key, model })).to.throw(Error, "tableName is not defined");
-    });
-
-    it('should throw an error with an invalid \'tableName\' argument', function() {
-      expect(()=>dbio.registerCollection({ key, model, tableName: null })).to.throw(Error, "tableName is null");
-    });
-
-    it('should throw an error with an invalid \'tableName\' argument', function() {
-      expect(()=>dbio.registerCollection({ key, model, tableName: 123 })).to.throw(Error, "tableName is not of type string");
-    });
-
-    it('should throw an error with an invalid \'tableName\' argument', function() {
-      expect(()=>dbio.registerCollection({ key, model, tableName: '' })).to.throw(Error, "tableName length is not atleast 1");
-    });
-  })
   describe('#registerEntity', function() {
+    
+    const key = 'person';
+    
+    const registerEntity = function(...args) {
+      return function() {
+        return dbio.registerEntity(...args)
+      }
+    }
+
+    afterEach(function(){
+      delete dbio[key];
+    })
+
     it('should be a function', function() {
       expect(dbio.registerEntity).to.be.a('function');
-    })
-  })
-})
+    });
+
+    it('should throw an error when given incorrect arguments', function() {
+      expect(registerEntity({ key })).to.throw(Error, "model is not defined");
+      expect(registerEntity({ model: null, key })).to.throw(Error, "model is null");
+      expect(registerEntity({ model: 123, key })).to.throw(Error, "model is not of type array");
+      expect(registerEntity({ model: [null], key })).to.throw(Error, "prop is null");
+      expect(registerEntity({ model: [{ key: null, column: 'name', type: 'TEXT' }], key })).to.throw(Error, "key is not of type string");
+      expect(registerEntity({ model: [{ key: '', column: 'name', type: 'TEXT' }], key })).to.throw(Error, "key length is not atleast 1");
+      expect(registerEntity({ model: [{ key: 123, column: 'name', type: 'TEXT' }], key })).to.throw(Error, "key is not of type string");
+      expect(registerEntity({ model: [{ key: 'name', column: null, type: 'TEXT' }], key })).to.throw(Error, "column is not of type string");
+      expect(registerEntity({ model: [{ key: 'name', column: '', type: 'TEXT' }], key })).to.throw(Error, "column length is not atleast 1");
+      expect(registerEntity({ model: [{ key: 'name', column: 123, type: 'TEXT' }], key })).to.throw(Error, "column is not of type string");
+      expect(registerEntity({ model: [{ key: 'name', column: 'name', type: null }], key })).to.throw(Error, "type is not of type string");
+      expect(registerEntity({ model: [{ key: 'name', column: 'name', type: '' }], key })).to.throw(Error, "type is not an allowed value");
+      expect(registerEntity({ model: [{ key: 'name', column: 'name', type: 123 }], key })).to.throw(Error, "type is not of type string");
+      expect(registerEntity({ model: [{ key: 'name', column: 'name', type: 'asdf' }], key })).to.throw(Error, "type is not an allowed value");
+      expect(registerEntity({ model })).to.throw(Error, "key is not defined");
+      expect(registerEntity({ key: null, model })).to.throw(Error, "key is null");
+      expect(registerEntity({ key: 123, model })).to.throw(Error, "key is not of type string");
+      expect(registerEntity({ key: '', model })).to.throw(Error, "key length is not atleast 1");
+    });
+
+    it('should register valid arguments without error', function() {
+      const factoryConstructor = dbio.registerEntity({ key, model, tableName });
+      expect(factoryConstructor).to.be.an.instanceof(Function);
+    });
+
+    it('should correctly register all properties', function() {
+    });
+  });
+});
 
 // const person = [
 //   {
